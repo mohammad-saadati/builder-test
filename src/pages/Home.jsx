79cc3a01df41b0d5d81f36cafb4fe8fe5f5f3eea
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-24 19:41:18 +0330

    useRef to catch element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 function Home() {
+  const [hirarcy, setHierarchy] = useState(null)
   return (
     <div className="w-full min-h-screen flex gap-10">
       <aside className="w-72 bg-slate-900"></aside>
       <iframe
+        ref={iframeRef}
         className="flex flex-1 m-10 rounded-2xl shadow-lg"
         src="/template"
       />
